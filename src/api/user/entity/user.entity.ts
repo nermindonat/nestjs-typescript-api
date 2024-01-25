ded2cc0dcc32f8ce7e83d2eea863093a4cc01804
Nermin Donat <nermindonat@posta.mu.edu.tr>
2024-01-25 17:59:55 +0300

    user entity and user dto created.

---
+++ src/api/user/entity/user.entity.ts
@@ -0,0 +1,11 @@
+export class User {
+    id: number;
+    email: string;
+    password?: string;
+    name: string;
+    surname: string;
+    country?: string;
+    province?: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
---
+++ src/api/user/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+import { DBService } from "src/database/DB.service";
+import { CreateUserDto } from "./dto/createUser.dto";
+
+@Injectable()
+export class UserService {
+    constructor (private readonly DBService: DBService) {}
+
+    async create(payload: CreateUserDto) {
+        const item = (await this.DBService.user.create({
+            data: payload
+        }))
+        return item
+    }
+}
